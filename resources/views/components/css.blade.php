018e969a8f67bb57d7b40afc7a242546b9f7e6dc
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2021-03-20 02:11:52 +0600

    Header Created

--- resources/views/components/css.blade.php
+++ resources/views/components/css.blade.php
@@ -1,7 +1,8 @@
 
 		<!-- google fonts -->
 		<link rel="preconnect" href="https://fonts.gstatic.com">
-		<link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;900&display=swap" rel="stylesheet">
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Bangers&family=Fredoka+One&family=Monoton&family=Pacifico&display=swap" rel="stylesheet">
 		<!-- Downloaded min css -->
         <link rel="stylesheet" href="{{ asset('/assets/css/bootstrap.min.css') }}">
         <link rel="stylesheet" href="{{ asset('/assets/css/fontawesome.min.css') }}">
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="head-div">
+        <div class="shade">
+            <div class="container">
+                <div class="app-title">
+                    <p><span>BUZZ</span> Love Calculator</>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
