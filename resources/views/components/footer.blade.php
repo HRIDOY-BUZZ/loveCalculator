88c82f770799a3493aef80a2c1e12711fcde9c61
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2021-03-20 02:38:14 +0600

    Footer and table-div created

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="footer-div">
+        <div class="shade-b">
+            <div class="container">
+                <div class="copyright-text">
+                    <p>
+                        <b>BUZZ LOVE CALCULATOR v3.0</b><br>
+                        © Copyright 2021 <a href="#"><i>HRIDOY-BUZZ</i></a>. All rights reserved.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,5 +6,20 @@
 
 
 @section('content')
+<section class="form-sec">
+    <div class="row">
+        <div class="col-xl-3 col-lg-3 col-md-2 col-sm-1">
 
+        </div>
+        <div class="col-xl-6 col-lg-6 col-md-10 col-sm-11">
+            <div class="form-div">
+
+            </div>
+        </div>
+        <div class="col-xl-3 col-lg-3 col-md-2 col-sm-1">
+
+        </div>
+    </div>
+
+</section>
 @endsection
